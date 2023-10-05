a8e264df8127bb1883aa0362e426b3927aede99c
Feng Shuowei <134953777+sekwai@users.noreply.github.com>
2023-10-05 01:53:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'PHP', 'Python'];
+    var member = {
+        name: 'Nguyen Van A',
+        age: 20,
+        skills: skills
+    };
+    console.log(member.skills);
+    console.log(member.skills[1]);
+    console.log(member.skills[3]);
+}
